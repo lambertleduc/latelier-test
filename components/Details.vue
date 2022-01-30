5a98212a40ea247806619ffd8098e9e25dd8927e
Lambert <l7474@tutanota.com>
2022-01-30 22:50:06 +0100

    FEAT: Adding Details panel

--- components/Details.vue
+++ components/Details.vue
@@ -1,26 +1,60 @@
 <template>
     <div id="details-section">
         <div v-for="(content, index) in players" :key="index">
-            <div v-for="(player, index) in content" :key="index" :id="player.firstname + player.id" class="details-modal">
-                <div class="col-5 player-img-section">
+            <div v-for="(player, index) in content" :key="index" :id="player.firstname + player.id" class="player-container">
+                <div class="col col-xl-5 player-stat-img">
                     <img :src="player.picture" class="w-100">
                 </div>
-                <div class="col-7 d-flex flex-column justify-content-around">
-                    <h2>{{ player.firstname }} {{ player.lastname }}</h2>
-                        <div class="row">
-                        <div class="col-4">
-                        <h4>Rank</h4>
-                        <p>#{{ player.data.rank }}</p>
+                <div class="player-stat-text col col-xl-9">
+                    <div class="player-stat-top">
+                        <div>
+                            <h1>{{ player.firstname }}</h1> 
+                            <h1>{{ player.lastname }}</h1>
                         </div>
-                        <div class="col-4">
-                            <h4>Points</h4>
-                            <p>{{ player.data.points }}</p>
+                        <div class="col col-xl-3"><img :src="player.country.picture" class="w-100"></div>
+                    </div>
+                    <div class="player-stat-description">
+                        <div>
+                            <div class="row">
+                                <div class="col-4">
+                                    <h4>Rank</h4>
+                                    <p>#{{ player.data.rank }}</p>
+                                </div>
+                                <div class="col-4">
+                                    <h4>Points</h4>
+                                    <p>{{ player.data.points }}</p>
+                                </div>
+                                <div class="col-4">
+                                    <h4>Country</h4>
+                                    <p>{{ player.country.code }}</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-4">
+                                    <h4>Birthday</h4>
+                                    <p>{{ getBirthday(player.data.age) }}</p>
+                                </div>
+                                <div class="col-4">
+                                    <h4>Age</h4>
+                                    <p>{{ player.data.age }}</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-4">
+                                    <h4>Rank</h4>
+                                    <p>#{{ player.data.weight }}</p>
+                                </div>
+                                <div class="col-4">
+                                    <h4>Points</h4>
+                                    <p>{{ player.data.height }}</p>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-4">
-                            <h4>Country</h4>
-                            <p>{{ player.country.code }}</p>
+                        <div>
+                            <h3 class="career-titles">Career Titles</h3>
+
                         </div>
-                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -35,6 +69,11 @@ export default{
         return{
             players
         } 
+    },
+    methods: {
+        getBirthday(age) {
+            return new Date().getFullYear() - age
+        }
     }
 }
 </script>
